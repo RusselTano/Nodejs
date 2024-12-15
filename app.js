0f2d8c0a8ad3a041976f0d2be0c74867351f404f
Dylane <tanolamene@gmail.com>
2024-12-15 14:29:31 -0400

    Fin Chapitre 4 : Le système de fichiers

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const fs = require("fs");
 
-// fs.open("./test.txt", "a+", (err, fd) => {
-//   //erreur et file descriptor (fd)
-//   if (err) throw err; //si erreur, on la lance
-//   fs.write(fd, "Hello World !", (err, written, str) => {
-//     console.log({ err, written, str }); //on affiche les erreurs, le nombre de caractères écrits et la chaine de caractères
-//     fs.close(fd, (err) => {});
-//   }); //on écrit dans le fichier
-// });
+fs.mkdir("./newFolder/subFolder",{recursive: true}, (err) => {});
 
+fs.readdir("./newFolder", (err, files) => {});
 
-fs.readFile("./test.txt", (err, data) => {
-  console.log(data.toString());
-})
-fs.appendFile("./test.txt", "Bye", (err) => {});
-// fs.unlink("./test.txt", (err) => {}); //supprime le fichier
-fs.readFile("./test.txt", (err, data) => {
-  console.log(data.toString());
-})
+// fs.rmdir("./newFolder", (err) => {});
+fs.rm("./newFolder/subFolder", {recursive: true}, (err) => {});
+
+fs.stat('./test.txt', (err, stat) => console.log(stat.isFile()));
