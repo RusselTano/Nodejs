5e750006f57f016dc826e35d856748990c160868
Dylane <tanolamene@gmail.com>
2024-12-15 12:03:55 -0400

    Debut Chapitre 4 : Le système de fichiers

--- events.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  FILE_OPENED: "fileOpened",
-  FILE_READ: "fileRead",
-  WRITTING_FILE: "writtingFile",
-  FILE_CLOSED: "fileClosed",
-};
--- module1.js
+++
@@ -1,6 +0,0 @@
-const myModule = () => console.log("Module 1 loaded");
-
-// myModule();
-
-module.exports = myModule; // This is the correct way to export a module
-
