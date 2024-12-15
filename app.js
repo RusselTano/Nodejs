f59852bba8a8bc2ce96e6ddec0dba35714f72e68
Dylane <tanolamene@gmail.com>
2024-12-15 07:30:09 -0400

    Evenement personaliser
    Creation des fichiers Emitter.js et event.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const myModule = require("./module1");
 const colors = require("colors");
 const util = require("util");
+const events = require("./events");
+const Emitter = require("./Emitter");
+
 const test = "Hello World";
 console.log(test, colors.red(test));
 
 myModule();
 console.log(util.types.isDate(123));
 console.log("yowoewowow".rainbow);
+
+// emitter and events
+const emitter = new Emitter();
+
+emitter.on(events.FILE_OPENED, () => console.log("File opened"));
+emitter.on(events.FILE_READ, () => console.log("File read"));
+
+emitter.emit(events.FILE_OPENED);
+
---
+++ events.js
@@ -0,0 +1,6 @@
+module.exports = {
+  FILE_OPENED: "fileOpened",
+  FILE_READ: "fileRead",
+  WRITTING_FILE: "writtingFile",
+  FILE_CLOSED: "fileClosed",
+};
