5f23fcb9768869f03d494b64a388585c9ae13d39
Dylane <tanolamene@gmail.com>
2024-12-16 19:27:21 -0400

    Premier serveur Express

---
+++ app.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const app = express(); // Express app
+// const port = 3500;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World !');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}. access http://localhost:${port}`);
+// });
