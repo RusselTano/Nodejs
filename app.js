3b71c054c10c50651e66f0924bbbc8d7d9cf476a
Dylane <tanolamene@gmail.com>
2024-12-17 13:09:20 -0400

    End Chapitre 8 : Les middleware sur Express

--- app.js
+++ app.js
@@ -2,21 +2,17 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = 3500;
+const port = 3300;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use("/foo", (req, res, next) => {
-  console.log("middleware1");
-  next();// permet de passer au middleware suivant
-});
-app.use("/foo", (req, res, next) => {
-  console.log("middleware2");
-  next();
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/foo", (req, res) => {
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
   res.render("index", {
     name: "Dylane",
     authenticated: true,
@@ -30,8 +26,20 @@ app.get("/foo", (req, res) => {
   });
 });
 
+
+
 app.get("/about", (req, res) => {
   res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+// app.use(express.urlencoded({ extended: false }));
+app.post("/contact", (req, res) => {
+  console.log("req.body:", req.body);
+  res.send("Merci pour votre message");
 });
 
 app.listen(port).on("listening", () => {
