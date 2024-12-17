237c91c58e26238e57f23774a1e35e858e9d4ac0
Dylane <tanolamene@gmail.com>
2024-12-17 12:24:14 -0400

    middleware

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3500;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use("/foo", (req, res, next) => {
+  console.log("middleware1");
+  next();// permet de passer au middleware suivant
+});
+app.use("/foo", (req, res, next) => {
+  console.log("middleware2");
+  next();
+});
+
+app.get("/foo", (req, res) => {
+  res.render("index", {
+    name: "Dylane",
+    authenticated: true,
+    friends: ["Alice", "Bob", "Charlie"],
+    friendsCount: 3,
+    products: [
+      { name: "Laptop", price: 1000 },
+      { name: "Smartphone", price: 500 },
+      { name: "Tablet", price: 300 },
+    ],
+  });
 });
 
 app.get("/about", (req, res) => {
